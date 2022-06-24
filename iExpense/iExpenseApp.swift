330f454512b9bbfbc87aaf6100f03348f3681d22
NT <usedforfunny@gmail.com>
2022-06-24 17:24:21 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Alexandre Talatinian on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
